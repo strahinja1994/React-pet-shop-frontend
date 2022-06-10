f1adc0b540bdee2309c812e81f72ac1d22bd2f11
strahinja1994 <82164543+strahinja1994@users.noreply.github.com>
2022-06-10 14:11:09 +0200

    Update Home.js

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,10 @@
 import React from 'react';
 import Main from '../Main';
-import Footer from '../Footer';
 
 export default function Home() {
     return(
         <div>
             <Main />
-            <Footer />
         </div>
     )
 }
